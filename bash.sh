00dbea446dcf1b48c12af92f4e7c03b6fa732825
Lucas Daniel Marín <116664832+lucasmarinskiba@users.noreply.github.com>
2025-05-13 21:46:14 -0300

    Update bash.sh

--- bash.sh
+++ bash.sh
@@ -3,3 +3,6 @@ git add .
 git commit -m "Initial commit"
 git remote add origin https://github.com/lucasmarinskiba/EnterpriseFlow.git
 git push -u origin main
+# Con PyInstaller (ejecuta en tu terminal):
+pip install pyinstaller
+pyinstaller --onefile --windowed main.py
