f272c9108b42c62533939c47ea5ec85bf29ec16c
Lucas Daniel Marín <116664832+lucasmarinskiba@users.noreply.github.com>
2025-05-13 22:13:05 -0300

    Update bash.sh

--- bash.sh
+++ bash.sh
@@ -6,3 +6,5 @@ git push -u origin main
 # Con PyInstaller (ejecuta en tu terminal):
 pip install pyinstaller
 pyinstaller --onefile --windowed main.py
+streamlit run main.py
+python -m spacy download es_core_news_sm
