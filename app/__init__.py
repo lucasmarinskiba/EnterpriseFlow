83b43b813ce6f2f4ae22dd65a14ba42cc5fc99f0
Lucas Daniel Marín <116664832+lucasmarinskiba@users.noreply.github.com>
2025-05-21 17:32:21 -0300

    Update __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -1,4 +1,13 @@
 # app/__init__.py
 from app.chatbot.routes import chatbot_bp
 
+# app/__init__.py
+from flask import Flask
+from dotenv import load_dotenv
+
+load_dotenv()  # Carga variables del .env
+
+app = Flask(__name__)
+app.config["DEEPSEEK_API_KEY"] = os.getenv("DEEPSEEK_API_KEY")
+
 app.register_blueprint(chatbot_bp, url_prefix='/chatbot')
